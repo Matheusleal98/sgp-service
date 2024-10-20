83252a3e18e1f9c2e16778861708c9cadb04be1f
Matheus Leal <matheussleal98@gmail.com>
2024-10-19 21:47:28 -0300

    feat(entities): criar classes entidades

---
+++ src/main/java/com/leal/sgp/model/pedido/Pedido.java
@@ -0,0 +1,97 @@
+package com.leal.sgp.model.pedido;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@Table(name = "PEDIDO")
+public class Pedido {
+
+    @Id
+    private UUID id;
+    @Column(name = "TX_ENT", precision = 10, scale = 2)
+    private BigDecimal taxaEntrega;
+    @Column(name = "TP_ENT_MIN")
+    private int tempoEntrega;
+    @Column(name = "SUBT_PRECO", precision = 10, scale = 2)
+    private BigDecimal subtotalPrice;
+    @Column(name = "T_DESCONTO", precision = 10, scale = 2)
+    private BigDecimal totalDesconto;
+    @Column(name = "STATUS")
+    private String status;
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "DT_PEDIDO")
+    private Date dataPedido;
+
+    public Pedido(){}
+
+    public Pedido(BigDecimal taxaEntrega, int tempoEntrega,
+                  BigDecimal subtotalPrice, BigDecimal totalDesconto,
+                  String status, Date dataPedido) {
+        this.taxaEntrega = taxaEntrega;
+        this.tempoEntrega = tempoEntrega;
+        this.subtotalPrice = subtotalPrice;
+        this.totalDesconto = totalDesconto;
+        this.status = status;
+        this.dataPedido = dataPedido;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public BigDecimal getTaxaEntrega() {
+        return taxaEntrega;
+    }
+
+    public void setTaxaEntrega(BigDecimal taxaEntrega) {
+        this.taxaEntrega = taxaEntrega;
+    }
+
+    public int getTempoEntrega() {
+        return tempoEntrega;
+    }
+
+    public void setTempoEntrega(int tempoEntrega) {
+        this.tempoEntrega = tempoEntrega;
+    }
+
+    public BigDecimal getSubtotalPrice() {
+        return subtotalPrice;
+    }
+
+    public void setSubtotalPrice(BigDecimal subtotalPrice) {
+        this.subtotalPrice = subtotalPrice;
+    }
+
+    public BigDecimal getTotalDesconto() {
+        return totalDesconto;
+    }
+
+    public void setTotalDesconto(BigDecimal totalDesconto) {
+        this.totalDesconto = totalDesconto;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Date getDataPedido() {
+        return dataPedido;
+    }
+
+    public void setDataPedido(Date dataPedido) {
+        this.dataPedido = dataPedido;
+    }
+}
---
+++ src/main/java/com/leal/sgp/model/produto/Produto.java
@@ -0,0 +1,86 @@
+package com.leal.sgp.model.produto;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name = "PRODUTO")
+public class Produto {
+
+    @Id
+    private UUID id;
+    @Column(name = "NOME")
+    private String nome;
+    @Column(name = "DESCRICAO")
+    private String descricao;
+    @Column(name = "IMG_URL")
+    private String imagemUrl;
+    @Column(name = "PRECO", precision = 10, scale = 2)
+    private BigDecimal preco;
+    @Column(name = "DESCONTO")
+    private int desconto;
+
+    public Produto(){}
+
+    public Produto(String nome, String descricao, String imagemUrl,
+                   BigDecimal preco, int desconto) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.imagemUrl = imagemUrl;
+        this.preco = preco;
+        this.desconto = desconto;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getImagemUrl() {
+        return imagemUrl;
+    }
+
+    public void setImagemUrl(String imagemUrl) {
+        this.imagemUrl = imagemUrl;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    public int getDesconto() {
+        return desconto;
+    }
+
+    public void setDesconto(int desconto) {
+        this.desconto = desconto;
+    }
+}
